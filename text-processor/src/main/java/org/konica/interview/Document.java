dd659e13df0fdc194dad435069178dd700b88388
xlatt <burianpato@gmail.com>
2019-05-14 17:50:17 +0200

    Add getter method for paragraphs
    so it is possible to serialize document with paragraphs. Without this
    document is saved to DB without paragraphs. This will cause problems
    when document is loaded and deserialized.

--- text-processor/src/main/java/org/konica/interview/Document.java
+++ text-processor/src/main/java/org/konica/interview/Document.java
@@ -78,6 +78,10 @@ public class Document {
         return wordFrequency;
     }
 
+    public ArrayList<String> getParagraphs(){
+        return paragraphs;
+    }
+
     private void splitByParagraphs() {
         if (content == null)
             return;
